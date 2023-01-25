734816e461c50cbbc0f5f2011cca814a0f1eed0e
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-01-25 15:59:10 -0300

    Rodar novamente avaliador github

--- app/backend/src/routes/LeaderBoard.ts
+++ app/backend/src/routes/LeaderBoard.ts
@@ -4,5 +4,6 @@ import LeaderBoardController from '../controller/LeaderBoard.controller';
 const leaderRoute = Router();
 
 leaderRoute.get('/home', LeaderBoardController.leaderBoard);
+leaderRoute.get('/away');
 
 export default leaderRoute;
